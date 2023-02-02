70d885cc827ab657c65ce33c46732fefb90ede04
Pavel <sozonovps@gmail.com>
2023-02-02 16:07:17 +0800

    Добавьте файлы проекта.

---
+++ task1/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class BankAccount<T>
+    {
+        public T Account { get; set; }
+        public double Balance { get; set; }
+        public string ClientName { get; set; }
+
+        //public BankAccount(T account, double balance, string clientName)
+        //{
+        //    Account = account;
+        //    Balance = balance;
+        //    ClientName = clientName;
+        //}
+
+        public string GetInfo()
+        { return $"{ClientName} имеет счет № {Account} с балансом {Balance}"; }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Account = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите ФИО клиента");
+            ClientName = Console.ReadLine();
+            Console.WriteLine("Введите баланс клиента");
+            Balance = Convert.ToDouble(Console.ReadLine());
+
+        }
+        public void InputBalance()
+        {
+            Console.WriteLine("Введите баланс клиента");
+            Balance = Convert.ToDouble(Console.ReadLine());
+        }
+        public void AddBalance()
+        {
+            Console.WriteLine("Введите изменение баланса клиента");
+            Balance += Convert.ToDouble(Console.ReadLine());
+        }
+
+    }
+
+
+}
---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<string> account1 = new BankAccount<string>();
+            account1.Input();
+            Console.WriteLine(account1.GetInfo());
+            BankAccount<int> account2 = new BankAccount<int>();
+            account2.Input();
+            Console.WriteLine(account2.GetInfo());
+            account2.AddBalance();
+            Console.WriteLine(account2.GetInfo());
+
+            Console.ReadLine();
+        }
+    }
+}
